4d8d946164b751348321bae3aff0e2dac6a90a88
Fisher <afishe08@harris.com>
2019-09-23 13:21:19 -0400

    adding power step test

---
+++ src/Device_Tests/exfo_t100_pm_power_step_test.cpp
@@ -0,0 +1,6 @@
+#include "exfo_t100_pm_power_step_test.h"
+
+EXFO_T100_PM_Power_Step_Test::EXFO_T100_PM_Power_Step_Test()
+{
+
+}
---
+++ src/Device_Tests/exfo_t100_pm_power_step_test.h
@@ -0,0 +1,11 @@
+#ifndef EXFO_T100_PM_POWER_STEP_TEST_H
+#define EXFO_T100_PM_POWER_STEP_TEST_H
+
+
+class EXFO_T100_PM_Power_Step_Test
+{
+public:
+    EXFO_T100_PM_Power_Step_Test();
+};
+
+#endif // EXFO_T100_PM_POWER_STEP_TEST_H
